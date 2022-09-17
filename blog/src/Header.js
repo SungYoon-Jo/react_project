e60f6fb5e1860c8cc1e027b611f3ff5a0bced20e
Jo  Seongyun <whtjddbs0112@naver.com>
2022-09-17 17:17:57 +0900

    헤더 만들기

--- blog/src/Header.js
+++ blog/src/Header.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
         <div className='header'>
-            <img className='header_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png'></img>
+            <img className='header_logo' src="img/logo.png"></img>
 
             <div className='header_search'>
-                <input className='header_earchInput' type="text"></input>
+                <input className='header_searchInput' type="text"></input>
+                <SearchIcon className='header_searchIcon' />
             </div>
+
             <div className='header_nav'>
                 <div className='header_option'>
                     <span className='header_optionLineOne'>안녕하세요 </span>
-                    <span className='header_optionLineOne'>로그인하기</span>
+                    <span className='header_optionLineTwo'>로그인하기</span>
                 </div>
 
                 <div className='header_option'>
                     <span className='header_optionLineOne'>돌아가기 </span>
-                    <span className='header_optionLineOne'>주문내역</span>
+                    <span className='header_optionLineTwo'>주문내역</span>
 
                 </div>
 
                 <div className='header_option'>
                     <span className='header_optionLineOne'>반가워요 </span>
-                    <span className='header_optionLineOne'>하이하이</span>
+                    <span className='header_optionLineTwo'>하이하이</span>
 
                 </div>
+
+                <div className='header_optionBasket'>
+                    <ShoppingBasketIcon/>
+                    <span className='header_optionLineTwoheader_basketCount'>
+                        0
+                    </span>
+                </div>
+
             </div>
         </div>
 
